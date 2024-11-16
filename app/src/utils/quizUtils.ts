dd2191410f857c89b7e32da109b81d59b67969ec
Zouaoui Mahjoub <saif.zouaoui-mahjoub@etu.unistra.fr>
2024-11-16 15:36:04 +0100

    mise a jour de quizUtils pour qu'il utilise des quizGame et non des quiz

--- app/src/utils/quizUtils.ts
+++ app/src/utils/quizUtils.ts
@@ -1,9 +1,6 @@
 import { prisma } from "../model/db";
 import { humanId } from "human-id";
 
-
-// fonction pour créer un ID  quiz
-// Retourne un ID unique pour un quiz
 export async function getUniqueId() {
 
     const allowedAttempts = 10;
@@ -14,7 +11,7 @@ export async function getUniqueId() {
             capitalize: false
         });
 
-        let quiz = await prisma.quiz.findUnique({
+        let quiz = await prisma.quizGame.findUnique({
             where: {
                 id: id
             }
@@ -28,12 +25,9 @@ export async function getUniqueId() {
     throw new Error("Impossible de générer un identifiant unique pour le quiz");
 }
 
-//Fonction pour réinitialiser le quiz
-// Recoit un id de quiz en paramètre sous forme de string
-// Retourne un objet JSON contenant les questions du quiz réinitialisé
 export async function resetProgress(quizId: string) {
 
-    await prisma.quiz.update({
+    await prisma.quizGame.update({
         where: {
             id: quizId
         },
@@ -42,12 +36,14 @@ export async function resetProgress(quizId: string) {
         }
     });
 
-    await prisma.question.updateMany({
+    await prisma.quizGameResponse.updateMany({
         where: {
-            quizId: quizId
+            quizGame: {
+                id: quizId
+            }
         },
         data: {
-            wasCorrect: false
+            response: false
         }
     });
 }
